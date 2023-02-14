c52de6730da7c58b8fea123b6bab9266987adab6
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2023-02-14 08:28:14 +0700

    connectDB

--- server/src/controllers/home.js
+++ server/src/controllers/home.js
@@ -1,5 +1,14 @@
-const getHome = (req, res) => {
-  return res.render('home.ejs')
+import db from '../models/index'
+
+const getHome = async (req, res) => {
+  try {
+    const data = await db.User.findAll()
+    return res.render('home.ejs', {
+      data: JSON.stringify(data),
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export { getHome }
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import viewEngine from './config/viewEngine'
 import initRouter from './routes/init'
+import connect from './config/connectDB'
 require('dotenv').config()
 
 const app = express()
@@ -12,6 +13,8 @@ app.use(bodyParser.json())
 
 viewEngine(app)
 initRouter(app)
+
+connect()
 
 app.listen(PORT, () => {
   console.log('Server is listening on port ' + PORT)
