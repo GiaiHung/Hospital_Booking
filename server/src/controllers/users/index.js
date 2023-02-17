5aa82f8d36c49f4f117b44e4362b7d52cecd1bbf
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2023-02-17 20:49:33 +0700

    update and delete user

--- server/src/controllers/users/index.js
+++ server/src/controllers/users/index.js
@@ -2,9 +2,39 @@ const db = require('../../models')
 
 const getAllUsers = async (req, res) => {
   const data = await db.User.findAll({ raw: true })
-  res.status(200).render('user.ejs', {
+  res.status(200).render('user/user.ejs', {
     data,
   })
 }
 
-export { getAllUsers }
+const getUpdateUser = async (req, res) => {
+  const userId = req.query.id
+  if (!userId) {
+    res.status(500).json({ message: 'Not having any user id' })
+  }
+  const user = await db.User.findByPk(userId, { raw: true })
+  if (!user) {
+    res.status(400).json({ message: 'User not found' })
+  }
+
+  res.render('user/user-edit.ejs', {
+    data: user,
+  })
+}
+
+const updateUser = async (req, res) => {
+  const user = await db.User.findOne({ where: { id: req.body.id } })
+  if (!user) return res.status(500).json({ message: 'User not found' })
+  await user.update(req.body)
+  res.redirect('/api/v1/users')
+}
+
+const deleteUser = async (req, res) => {
+  const id = req.query.id
+  const user = await db.User.findByPk(id)
+  if (!user) return res.status(500).json({ message: 'User not found' })
+  await user.destroy()
+  res.redirect('/api/v1/users')
+}
+
+export { getAllUsers, getUpdateUser, updateUser, deleteUser }
--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,8 +1,17 @@
 import express from 'express'
-import { getAllUsers } from '../controllers/users'
+import {
+  deleteUser,
+  getAllUsers,
+  getUpdateUser,
+  updateUser,
+} from '../controllers/users'
 
 const router = express.Router()
 
 router.get('/', getAllUsers)
+router.get('/edit', getUpdateUser)
+router.post('/edit', updateUser)
+router.delete('/delete', deleteUser)
+router.get('/delete', deleteUser)
 
 export default router
